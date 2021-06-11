061179b0126c346ba8598eba9acfc67dc35fdc2f
Kai Chang <kaijchang@gmail.com>
2021-06-11 11:37:31 -0700

    responsive divider

--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -4,9 +4,10 @@ import Rough from './rough'
 const SectionDivider: React.FC<SVGAttributes<SVGSVGElement>> = (props) => (
 	<Rough
 		height={20}
-		width={368}
+		width="100%"
 		init={(rc, svg) => {
-			let node = rc.line(0, 10, 368, 10, {
+			const bounds = svg.getBoundingClientRect()
+			let node = rc.line(0, 10, bounds.width, 10, {
 				stroke: 'var(--chakra-colors-orange-400)',
 				roughness: 2.5,
 				bowing: 1.5,
